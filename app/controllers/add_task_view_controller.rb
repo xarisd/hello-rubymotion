5b42353162772085495a8ae4c564d739983b4305
xarisd <xaris.dimitriou@gmail.com>
2014-12-13 01:07:43 +0200

    Add textfield delegate to handle the return key as "save_task"

--- app/controllers/add_task_view_controller.rb
+++ app/controllers/add_task_view_controller.rb
@@ -4,10 +4,15 @@ class AddTaskViewController < UIViewController
   def loadView
     self.view = AddTaskView.new
     view.button.addTarget(self, action: :save_task, forControlEvents: UIControlEventTouchUpInside)
+    view.text_field.delegate = self
   end
 
   def viewDidLoad
     self.title = "Add Task"
+  end
+
+  def textFieldShouldReturn(text_field)
+    save_task
   end
 
   def save_task
